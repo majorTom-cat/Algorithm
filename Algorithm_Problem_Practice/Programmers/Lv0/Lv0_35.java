25665cc9f95864f5d4cdd37919bf1749b8baf7c6
majorTom-cat <ysk.skyer@gmail.com>
2023-07-12 02:06:32 +0900

    Update Lv0_35.java

--- Algorithm_Problem_Practice/Programmers/Lv0/Lv0_35.java
+++ Algorithm_Problem_Practice/Programmers/Lv0/Lv0_35.java
@@ -1,12 +1,12 @@
 package Lv0;
 
-import java.util.Arrays;
-
 public class Lv0_35 {
 
 	public static void main(String[] args) {
 		
-		int[][] fasd = {{80, 70}, {90, 50}, {40, 70}, {50, 80}};
+		//int[][] fasd = {{80, 70}, {90, 50}, {40, 70}, {50, 80}};
+		//int[][] fasd = {{0, 0}, {70, 80}, {30, 50}, {90, 100}, {100, 90}, {100, 100}, {0, 0}};
+		int[][] fasd = {{1, 3}, {3, 1}, {2, 3}, {3, 2}, {1, 2}, {0, 0}};
 		
 		solution(fasd);
 
@@ -15,22 +15,18 @@ public class Lv0_35 {
 	 public static int[] solution(int[][] score) {
         int[] answer = new int[score.length];
         
-        Arrays.sort(answer);
-        
-        Double[] avg = null;
-        
-        avg = new Double[score.length];
+        double[] avg = new double[score.length];
         
         for(int i = 0; i < score.length; i++) {
-        	int sum = score[i][0] + score[i][1];
-        	Double tempAvg = (double) (sum / 2);
-        	avg[i] = tempAvg; 
-        	System.out.println(avg[i]);
+        	double sum = score[i][0] + score[i][1];
+        	double tempAvg = sum / 2;
+        	avg[i] = tempAvg;
         }
         
-        Double max = 0.0;
+        double max = -0.5;
         int maxIndex = 0;
         int rank = 1;
+        int jointRank = 1;
         for(int i = 0; i < score.length; i++) {
         	
         	for(int j = 0; j < score.length; j++) { // 최대값 찾기
@@ -41,17 +37,37 @@ public class Lv0_35 {
         	}
         	
         	answer[maxIndex] = rank;
+        	avg[maxIndex] = -1.0;
         	
-        	for(int k = 0; k < score.length; k++) {
-        		if(max == avg[k]) {
-        			rank++;
+        	for(int k = 0; k < score.length; k++) { // 공동 순위 찾기
+        		
+        		if(k == maxIndex) {
+        			System.out.println("max : " + max + "    avg[k] : " + avg[k] + "    <------ 현재");
+        			continue;
+        		} else {
+        			System.out.println("max : " + max + "    avg[k] : " + avg[k]);
+        			if(max == avg[k]) {
+            			System.out.println("공동 순위 찾음. jointRank : " + jointRank);
+            			answer[k] = rank;
+            			avg[k] = -1.0;
+            			jointRank++;
+            			i++;
+            		}
         		}
+        		
         	}
+        	System.out.println();
+
+        	max = -0.5;
+        	rank += jointRank;
+        	jointRank = 1;
         	
+        	for(int l = 0; l < score.length; l++) {
+        		System.out.print(answer[l] + "  ");
+        	}
+        	System.out.println();
         }
-        
-        //System.out.println(answer[0]);
-        
+
         return answer;
 	    }
 
